8f7ad28618eebb16e64a987e188f76e75e079b03
Voicu Tibea <voicu.tibea@yopeso.com>
2019-01-21 16:11:54 +0200

    Update module
    Signed-off-by: Voicu Tibea <voicu.tibea@yopeso.com>

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+const DB_HOSTNAME = 'localhost';
+const DB_USERNAME = 'root';
+const DB_PASSWORD = 'R00tP@ss';
+const DB_DATABASE = 'api_generator';
---
+++ src/ApiGenerator/Generator.php
@@ -0,0 +1,10 @@
+<?php
+namespace ApiGenerator;
+
+class Generator
+{
+    public static function test()
+    {
+        return 'Api Generator!';
+    }
+}
